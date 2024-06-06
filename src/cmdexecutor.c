e9a1ac14aa50ebf41ec338b5a0607282040f4e75
realzvqle <realzvql@gmail.com>
2024-06-06 13:20:37 -0500

    0.1

--- src/cmdexecutor.c
+++ src/cmdexecutor.c
@@ -10,6 +10,16 @@ void execute_commands(int index, data* data){
             if(!data->arg) break;
             printf("%s\n", data->arg);
             break;
+        case 1:
+            if(!data->arg) break;
+            int result = chdir(data->arg);
+            if(result == -1){
+                perror("cd failed");
+            }
+            break;
+        case 2:
+            _exit(0);
+            break;
         default:
             printf("%s has been found, but no implemnation has been found\n", data->cmd);
             break;
--- src/cmdprocessor.c
+++ src/cmdprocessor.c
@@ -1,10 +1,12 @@
 #include "cmdprocessor.h"
 #include "cmdexecutor.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 
-
-static char* cmds[] = {"echo"};
+static char* cmds[] = {"echo", "cd", "exit"};
 
 
 
@@ -19,5 +21,37 @@ void process_commands(data* data){
             return;
         }
     }
-    printf("%s was not found\n", data->cmd);
+    
+    char* args[4096];
+    int argc = 0;
+    char* token = strtok(data->arg, " ");
+    while (token != NULL && argc < 4095) {
+        args[argc++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[argc] = NULL;
+
+    char* argv[argc + 2];
+    argv[0] = data->cmd;
+    for (int i = 0; i <= argc; ++i) {
+        argv[i + 1] = args[i];
+    }
+
+    int result = fork();
+    if(result == 0){
+        execvp(data->cmd, argv);
+        perror("Failed executing process");
+        _exit(-1);
+    }
+    else if(result == -1){
+        perror("couldn't fork process");
+
+    }
+    else{
+        int status;
+        waitpid(result, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            printf("%s was not found\n", data->cmd);
+        }
+    }
 }
